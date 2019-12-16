0a7b3234603fbc3bd438bc598cce88590ede728a
Neetshin <n33t5hin@gmail.com>
2019-12-17 00:28:18 +0900

    Use jest-puppeteer

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,19 @@
 module.exports = {
   projects: [
     {
-      globalSetup: "jest-environment-puppeteer/setup",
-      globalTeardown: "jest-environment-puppeteer/teardown",
-      testEnvironment: "jest-environment-puppeteer",
+      preset: 'jest-puppeteer',
       globals: {
         'ts-jest': {
-          tsConfig: '<rootDir>/packages/core/tsconfig.json'
+          tsConfig: '<rootDir>/packages/core/tsconfig.json',
         },
       },
       transform: { '^.+\\.tsx?$': 'ts-jest' },
-      testMatch: [
-        '<rootDir>/packages/core/**/?(*.)+(spec|test).ts?(x)',
-      ],
+      testMatch: ['<rootDir>/packages/core/**/?(*.)+(spec|test).ts?(x)'],
       testPathIgnorePatterns: ['/node_modules/'],
       moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
       collectCoverageFrom: ['<rootDir>/core/**/*.{ts,tsx}'],
       coverageDirectory: '<rootDir>/coverage',
       coveragePathIgnorePatterns: ['.*\\.d\\.ts'],
-    }
-  ]
+    },
+  ],
 };
