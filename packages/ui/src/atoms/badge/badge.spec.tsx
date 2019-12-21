8d05abd922709979097f047ab0b7e5029479ee08
Neetshin <n33t5hin@gmail.com>
2019-12-21 17:36:53 +0900

    Update tests

--- packages/ui/src/atoms/badge/badge.spec.tsx
+++ packages/ui/src/atoms/badge/badge.spec.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../theme';
 import { Badge } from '.';
 
 describe('Badge', () => {
   it('matches snapshot', () => {
-    const { container } = render(<Badge />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Badge />
+      </ThemeProvider>,
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
--- packages/ui/src/atoms/progress/progress.spec.tsx
+++ packages/ui/src/atoms/progress/progress.spec.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../theme';
 import { Progress } from '.';
 
 describe('Progress', () => {
   it('matches snapshot', () => {
-    const { container } = render(<Progress />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Progress progress={50} />
+      </ThemeProvider>,
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
