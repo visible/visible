3d1bcd8f1255c9e7e7630eccac3a1f08abdbec04
Neetshin <n33t5hin@gmail.com>
2019-12-23 18:22:32 +0900

    Setup react + apollo

--- packages/client/src/pages/root/root.tsx
+++ packages/client/src/pages/root/root.tsx
@@ -1,18 +1,45 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient } from 'apollo-client';
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher,
+} from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from 'styled-components';
+import typeDefs from '@visi/schema';
 import { theme } from '@visi/ui/dist/theme';
 import { Home } from '../home';
+import introspectionResult from '../../generated/introspection-result';
 
 export const Root = () => {
+  const fragmentMatcher = new IntrospectionFragmentMatcher({
+    introspectionQueryResultData: introspectionResult,
+  });
+
+  const cache = new InMemoryCache({ fragmentMatcher }).restore(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).__APOLLO_STATE__,
+  );
+
+  const client = new ApolloClient({
+    cache,
+    typeDefs,
+    link: new HttpLink({ uri: '/graphql' }),
+    ssrForceFetchDelay: 100,
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
