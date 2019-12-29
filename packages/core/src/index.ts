4757775c2bfe770519b7a77d989b7592dd4fb594
Neetshin <n33t5hin@gmail.com>
2019-12-29 21:41:13 +0900

    export core interfaces

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,47 +1,6 @@
-import puppeteer from 'puppeteer';
-import { rules } from './rules';
-import { Report } from './domain/report';
-import { Fixers } from './domain/fixers';
-import { Rule } from './domain/rule';
-import { createI18n } from './utils/i18n';
-import { Context } from './domain/context';
+export * from './domain/context';
+export * from './domain/fixers';
+export * from './domain/report';
+export * from './domain/rule';
 
-export interface VisibleParams {
-  readonly url?: string;
-  readonly html?: string;
-  readonly language?: string;
-  readonly rules?: Rule[];
-  readonly fixers?: Fixers;
-  // readonly i18n?: i18n;
-}
-
-export const visible = async (params: VisibleParams) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const [, t] = await createI18n(params.language);
-
-  if (params.url) {
-    await page.goto(params.url);
-  }
-
-  if (params.html) {
-    await page.setContent(params.html);
-  }
-
-  const reports: Report[] = [];
-  const context: Context = {
-    page,
-    t,
-    fixers: params.fixers,
-  };
-
-  for (const rule of rules.concat(params.rules || [])) {
-    const newReports = await rule(context);
-    reports.push(...newReports);
-  }
-
-  await page.close();
-  await browser.close();
-
-  return reports;
-};
+export * from './visible';
