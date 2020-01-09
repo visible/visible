e193988e7f486d737d896838a2e61716101b5af4
Neetshin <n33t5hin@gmail.com>
2020-01-09 12:50:27 +0900

    Add cspell

--- packages/client/src/pages/home/home.tsx
+++ packages/client/src/pages/home/home.tsx
@@ -65,13 +65,7 @@ export const Home = () => {
             </Trans>
           </Description>
           {loading && <UI.Progress progress={50} />}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          blah blah blah
         </Inner>
       </Wizard>
     </UI.Content>
