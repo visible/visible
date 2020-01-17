ca4662078cc4291038d5817219c41a7864be6ba5
Neetshin <n33t5hin@gmail.com>
2020-01-17 19:57:55 +0900

    Setup storybook w/o CRA properly

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,10 +1,17 @@
+import path from 'path';
+
 export default {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs',
-    '@storybook/preset-typescript',
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        include: [path.resolve(__dirname, '../src')],
+      },
+    },
     {
       name: '@storybook/addon-docs',
       options: {
--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,8 +1,6 @@
 import { addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withTheme } from '../src/with-theme';
 
-addDecorator(withInfo({ inline: true }));
 addDecorator(withKnobs);
 addDecorator(withTheme);
