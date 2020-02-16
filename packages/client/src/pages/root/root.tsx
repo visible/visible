418c2af10667753b8fd790858f32d81e29043b02
Neetshin <n33t5hin@gmail.com>
2020-02-16 13:39:19 +0900

    Remove suspense due to lack of support

--- packages/client/src/pages/root/root.tsx
+++ packages/client/src/pages/root/root.tsx
@@ -1,21 +1,25 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router';
 import { Banner } from '../../components/banner';
 import { ContentInfo } from '../../components/content-info';
-import { Home, Void, Diagnoses } from './lazy';
+
+import Home from '../home';
+import Diagnoses from '../diagnoses';
+import Void from '../void';
+
+// SuspenseがSSRで使えなかった...
+// import { Home, Void, Diagnoses } from './lazy';
 
 export const Root = () => {
   return (
     <>
       <Banner role="banner" />
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/diagnoses/:id" component={Diagnoses} />
-          <Route component={Void} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/diagnoses/:id" component={Diagnoses} />
+        <Route component={Void} />
+      </Switch>
 
       <ContentInfo role="contentinfo" />
     </>
