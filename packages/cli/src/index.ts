af334850c538338fbfa6a6e26cd1a09c91803b34
Neetshin <n33t5hin@gmail.com>
2020-02-29 13:35:31 +0900

    feat: Add init command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { visible } from '@visi/core/main';
 import { cosmiconfig } from 'cosmiconfig';
+import { promises as fs } from 'fs';
 import yargs from 'yargs';
 
 import { createI18n } from './i18n';
@@ -9,49 +10,58 @@ import { print } from './print';
 
 (async () => {
   const [, t] = await createI18n();
-  const description = t('visible.description', 'The default command');
 
-  yargs.command(
-    '*',
-    description,
-    yargs =>
-      yargs
-        .option('url', {
-          description: t('options.url', 'URL to diagnose'),
-          type: 'string',
-          required: true,
-        })
-        .option('json', {
-          description: t(
-            'options.json',
-            'Output JSON instead of prettified table',
-          ),
-          type: 'boolean',
-          default: false,
-        })
-        .option('fix', {
-          description: t('options.fix', 'Prints fixers'),
-          type: 'boolean',
-          default: false,
-        })
-        .option('verbose', {
-          description: t(
-            'options.verbose',
-            'Prints all reports including passed one',
-          ),
-          type: 'boolean',
-          default: false,
-        }),
+  yargs
+    .command(
+      'init',
+      t('visible.init.description', 'Initialize .visiblerc file') as string,
+      undefined,
+      async () => {
+        await fs.writeFile('.visiblerc.json', JSON.stringify({}));
+        // eslint-disable-next-line no-console
+        console.log(t('visible.init.done', '🎉 Initialization completed!!'));
+      },
+    )
+    .command(
+      '*',
+      t('visible.description', 'The default command') as string,
+      yargs =>
+        yargs
+          .option('url', {
+            description: t('options.url', 'URL to diagnose'),
+            type: 'string',
+            required: true,
+          })
+          .option('json', {
+            description: t(
+              'options.json',
+              'Output JSON instead of prettified table',
+            ),
+            type: 'boolean',
+            default: false,
+          })
+          .option('fix', {
+            description: t('options.fix', 'Prints fixers'),
+            type: 'boolean',
+            default: false,
+          })
+          .option('verbose', {
+            description: t(
+              'options.verbose',
+              'Prints all reports including passed one',
+            ),
+            type: 'boolean',
+            default: false,
+          }),
 
-    async ({ url, json, verbose, fix }) => {
-      try {
-        const cosmiconfigResult = await cosmiconfig('visible').search();
+      async ({ url, json, verbose, fix }) => {
+        const config = await cosmiconfig('visible')
+          .search()
+          .then(result => result?.config);
 
-        if (!cosmiconfigResult?.config) {
+        if (!config) {
           throw new Error(t('visible.no-rc', 'No visiblerc file found'));
         }
-
-        const { config } = cosmiconfigResult;
 
         const reports = await loader(
           t('visible.loading', 'Fetching diagnoses...'),
@@ -62,11 +72,6 @@ import { print } from './print';
 
         const hasError = reports.some(report => report.level === 'error');
         process.exit(hasError ? 1 : 0);
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error(e);
-        process.exit(1);
-      }
-    },
-  ).argv;
+      },
+    ).argv;
 })();
