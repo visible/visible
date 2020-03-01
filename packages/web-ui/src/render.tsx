c23821a8bcd3fab8d562d8fa5b08e006cc09a3ba
Neetshin <n33t5hin@gmail.com>
2020-03-01 17:58:25 +0900

    fix: Fix missing dependency

--- packages/web-ui/src/render.tsx
+++ packages/web-ui/src/render.tsx
@@ -1,7 +1,8 @@
 import { render as defaultRender } from '@testing-library/react';
-import { theme } from '@visi/resources';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+
+import { theme } from './__fixtures__/default-theme';
 
 const TestProvider: React.FC = props => {
   const { children } = props;
--- packages/web-ui/src/with-theme.tsx
+++ packages/web-ui/src/with-theme.tsx
@@ -1,25 +1,9 @@
 import { DecoratorFn } from '@storybook/react';
 import React from 'react';
 
+import { theme } from './__fixtures__/default-theme';
 import { ConfigProvider } from './config';
 import { GlobalStyle } from './global';
-
-const theme = {
-  foreground: {
-    normal: '#333333',
-    wash: '#666666',
-  },
-  background: {
-    normal: '#ffffff',
-    wash: '#f1f1f1',
-  },
-  highlight: {
-    normal: '#ffaa01',
-  },
-  border: {
-    normal: '#e6e6e6',
-  },
-};
 
 export const withTheme: DecoratorFn = storyFn => {
   return (
