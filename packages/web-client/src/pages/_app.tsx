690b79d7a5d07a4c9dc0749d12faa8d3a8acb7e5
Neetshin <n33t5hin@gmail.com>
2020-04-11 03:13:08 +0900

    Add <DefaultSeo />

--- packages/web-client/src/pages/_app.tsx
+++ packages/web-client/src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import { theme } from '@visi/resources';
 import { ConfigProvider, GlobalStyle } from '@visi/web-ui';
+import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 import React from 'react';
 
 import { Banner } from '../components/banner';
 import { ContentInfo } from '../components/content-info';
-import { appWithTranslation } from '../utils/i18next';
+import { appWithTranslation, useTranslation } from '../utils/i18next';
 
 type CustomAppProps = AppProps;
 
 const CustomApp = (props: CustomAppProps) => {
+  const { t } = useTranslation();
   const { Component, pageProps } = props;
 
   return (
     <ConfigProvider theme={theme}>
+      <DefaultSeo
+        titleTemplate={'%s - Visible'}
+        openGraph={{ type: 'website', site_name: t('meta.title', 'Visible') }}
+      />
       <GlobalStyle />
 
       <div role="application">
