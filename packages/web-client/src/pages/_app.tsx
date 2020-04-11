a1da0de7c56299d4f6bfcaae34932d6a48570b43
Neetshin <n33t5hin@gmail.com>
2020-04-11 12:10:37 +0900

    fix: Restore manifest.json

--- packages/web-client/src/pages/_app.tsx
+++ packages/web-client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { theme } from '@visi/resources';
 import { ConfigProvider, GlobalStyle } from '@visi/web-ui';
 import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 
 import { Banner } from '../components/banner';
@@ -13,14 +14,31 @@ type CustomAppProps = AppProps;
 const CustomApp = (props: CustomAppProps) => {
   const { t } = useTranslation();
   const { Component, pageProps } = props;
+  const title = t('meta.title', 'Visible');
 
   return (
     <ConfigProvider theme={theme}>
       <DefaultSeo
         titleTemplate={'%s - Visible'}
-        openGraph={{ type: 'website', site_name: t('meta.title', 'Visible') }}
+        openGraph={{ type: 'website', site_name: title }}
       />
       <GlobalStyle />
+
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/api/manifest" />
+        <meta name="theme-color" content={theme.highlight.normal} />
+
+        <link
+          rel="mask-icon"
+          href="/static/logo.svg"
+          color={theme.highlight.normal}
+        />
+        <link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content={title} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </Head>
 
       <div role="application">
         <Banner role="banner" />
